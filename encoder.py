69d91c1e9e747eeceae457624b9669924cb33d29
delaney-seguin <145727041+delaney-seguin@users.noreply.github.com>
2023-10-25 20:22:43 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,1 +1,6 @@
-d
+def encoder(password):
+    encodedPW = ''
+    print("Your password has been encoded and stored!")
+    for i in range(len(password)):
+        encodedPW += str((int(password[i]) + 3)%10)
+    return encodedPW
