e665e9a4f27a3e62cf477e47e0dd2aac53591f99
delaney-seguin <145727041+delaney-seguin@users.noreply.github.com>
2023-10-25 20:19:14 -0400

    Update and rename EncoderLab6.py to mainLab6.py

--- mainLab6.py
+++ mainLab6.py
@@ -11,11 +11,5 @@ def main():
             print('The encoded password is ',encodedPW,'and the orignal password is ',decode(encodedPW))
         elif selection ==3:
             condition = False 
-def encoder():
-    password = input('Please enter your password to encode:')
-    encodedPW = ''
-    print("Your password has been encoded and stored!")
-    for i in range(len(password)):
-        encodedPW += str((int(password[i]) + 3)%10)
 if __name__ == "__main__":
     main()
