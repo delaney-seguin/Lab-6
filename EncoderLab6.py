99ee80d7ae780e29d3f3b7f52e52cc7a093bb60c
delaney-seguin <145727041+delaney-seguin@users.noreply.github.com>
2023-10-25 20:16:48 -0400

    Update EncoderLab6.py

--- EncoderLab6.py
+++ EncoderLab6.py
@@ -6,15 +6,16 @@ def main():
         print('-------------')
         selection = int(input('1. Encode \n2. Decode \n3. Quit\n\nPlease Enter an option: '))
         if selection == 1:
-            password = input('Please enter your password to encode:')
-            encodedPW = ''
-            print("Your password has been encoded and stored!")
-            for i in range(len(password)):
-                encodedPW += str((int(password[i]) + 3)%10)
+            encoder()
         elif selection == 2:
-            print("The encoded password is ", encodedPW, "and the original password is ", password,".")
+            print('The encoded password is ',encodedPW,'and the orignal password is ',decode(encodedPW))
         elif selection ==3:
             condition = False 
-def
+def encoder():
+    password = input('Please enter your password to encode:')
+    encodedPW = ''
+    print("Your password has been encoded and stored!")
+    for i in range(len(password)):
+        encodedPW += str((int(password[i]) + 3)%10)
 if __name__ == "__main__":
     main()
